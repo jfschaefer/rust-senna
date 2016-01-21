76450abcb690a44ec9f23cacc7aca9c419ad9a93
jfschaefer <j.schaefer@jacobs-university.de>
2016-01-21 17:47:01 +0100

    a first test case

---
+++ tests/lib.rs
@@ -0,0 +1,15 @@
+//! A few random tests.
+//! Knowing myself, the coverage will be insignificant ;)
+
+extern crate rustsenna;
+
+use rustsenna::senna::Senna;
+use rustsenna::tokenizer::Tokenizer;
+
+#[test]
+/// Tokenization returns correct number of words
+fn test_token_count() {
+    let mut senna = Senna::new("senna/");
+    assert_eq!(2, senna.get_number_of_words("Hello world"));
+}
+
