022092b0deb0915ecdddc4fb9f36ad44931df679
jfschaefer <j.schaefer@jacobs-university.de>
2016-01-21 11:50:08 +0100

    created build script for c_wrapper

---
+++ build.rs
@@ -0,0 +1,21 @@
+//! We'll have to compile senna and our c_wrapper.c to create libc_wrapper.a
+//! We assume that `make`, `gcc` and `ar` are installed and that the senna directory
+//! is located in the right place.
+//! The actual compilation is managed by the make file `wrapperMakefile`
+
+
+fn main() {
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+
+    std::process::Command::new("make")
+        .args(&["static_lib", "-f", "wrapperMakefile", &format!("OUT_DIR={}", out_dir), "SENNA_DIR=senna"])
+        .status().unwrap();
+    // `make clean` only removes *.o files
+    std::process::Command::new("make")
+        .args(&["clean", "-f", "wrapperMakefile", &format!("OUT_DIR={}", out_dir), "SENNA_DIR=senna"])
+        .status().unwrap();
+
+    println!("cargo:rustc-link-search=native={}", out_dir);
+    println!("cargo:rustc-link-lib=static=c_wrapper");
+}
+
